95127a049c7acf3fb5eb870fc5af51725e7a0ffc
Alexa <kusteralexa@gmail.com>
2022-11-09 19:28:22 -0500

    commit tree 302c2ce834b8318d273f74e988d78881b0f40e2b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+let dodgerR = document.getElementById("dodgerRight");
+
+function moveDodgerright() {
+  const leftNumbers = dodgerR.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodgerR.style.right = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerright();
+  }
+});
